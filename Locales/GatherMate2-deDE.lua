ad714c48da4ade4c2ceace4e1d69b62e75c9d101
kaiuwepeter <sidekix79@gmail.com>
2025-11-18 23:36:27 +0100

    Add German translations for new midnight nodes

--- Locales/GatherMate2-deDE.lua
+++ Locales/GatherMate2-deDE.lua
@@ -11,3 +11,54 @@ local NL = LibStub("AceLocale-3.0"):NewLocale("GatherMate2Nodes", "deDE")
 if not NL then return end
 
 --@localization(locale="deDE", format="lua_additive_table", namespace="Nodes", table-name="NL", handle-unlocalized="ignore")@
+
+-- Midnight Expansion Nodes (Custom)
+NL["Argentleaf"] = "Argentumblatt"
+NL["Wild Argentleaf"] = "Wildes Argentumblatt"
+NL["Lush Argentleaf"] = "Üppiges Argentumblatt"
+NL["Voidbound Argentleaf"] = "Leerengebundenes Argentumblatt"
+NL["Lightfused Argentleaf"] = "Lichtverschmolzenes Argentumblatt"
+NL["Primal Argentleaf"] = "Urtümliches Argentumblatt"
+
+NL["Mana Lily"] = "Manalilie"
+NL["Wild Mana Lily"] = "Wilde Manalilie"
+NL["Lush Mana Lily"] = "Üppige Manalilie"
+NL["Voidbound Mana Lily"] = "Leerengebundene Manalilie"
+NL["Lightfused Mana Lily"] = "Lichtverschmolzene Manalilie"
+NL["Primal Mana Lily"] = "Urtümliche Manalilie"
+
+NL["Tranquility Bloom"] = "Harmonieblume"
+NL["Wild Tranquility Bloom"] = "Wilde Harmonieblume"
+NL["Lush Tranquility Bloom"] = "Üppige Harmonieblume"
+NL["Voidbound Tranquility Bloom"] = "Leerengebundene Harmonieblume"
+NL["Lightfused Tranquility Bloom"] = "Lichtverschmolzene Harmonieblume"
+NL["Primal Tranquility Bloom"] = "Urtümliche Harmonieblume"
+
+NL["Refulgent Copper"] = "Glänzendes Kupfer"
+NL["Refulgent Copper Seam"] = "Glänzendes Kupferflöz"
+NL["Voidbound Refulgent Copper"] = "Leerengebundenes glänzendes Kupfer"
+NL["Lightfused Refulgent Copper"] = "Lichtverschmolzenes glänzendes Kupfer"
+NL["Rich Refulgent Copper"] = "Reiches glänzendes Kupfer"
+NL["Primal Refulgent Copper"] = "Urtümliches glänzendes Kupfer"
+NL["Wild Refulgent Copper"] = "Wildes glänzendes Kupfer"
+
+NL["Umbral Tin"] = "Umbralzinn"
+NL["Umbral Tin Seam"] = "Umbralzinnflöz"
+NL["Voidbound Umbral Tin"] = "Leerengebundenes Umbralzinn"
+NL["Lightfused Umbral Tin"] = "Lichtverschmolzenes Umbralzinn"
+NL["Rich Umbral Tin"] = "Reiches Umbralzinn"
+NL["Primal Umbral Tin"] = "Urtümliches Umbralzinn"
+NL["Wild Umbral Tin"] = "Wildes Umbralzinn"
+
+NL["Brilliant Silver"] = "Brillantes Silber"
+NL["Brilliant Silver Seam"] = "Brillantes Silberflöz"
+NL["Voidbound Brilliant Silver"] = "Leerengebundenes brillantes Silber"
+NL["Lightfused Brilliant Silver"] = "Lichtverschmolzenes brillantes Silber"
+NL["Rich Brilliant Silver"] = "Reiches brillantes Silber"
+NL["Primal Brilliant Silver"] = "Urtümliches brillantes Silber"
+NL["Wild Brilliant Silver"] = "Wildes brillantes Silber"
+
+NL["Hunter Surge"] = "Jägerwoge"
+NL["Surface Ripple"] = "Oberflächenkräuseln"
+NL["Bubbling Bloom"] = "Blubbernde Blüte"
+NL["Lost Treasures"] = "Verlorene Schätze"
