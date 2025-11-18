97610379131e69fb32042aa56d6b11fcded7792a
kaiuwepeter <sidekix79@gmail.com>
2025-11-18 23:37:18 +0100

    Add Midnight Expansion nodes to localization
    Added new nodes for the Midnight Expansion to the GatherMate2 localization file.

--- Locales/GatherMate2-enUS.lua
+++ Locales/GatherMate2-enUS.lua
@@ -14,3 +14,55 @@ local L = LibStub("AceLocale-3.0"):NewLocale("GatherMate2", "enUS", true, debug)
 local NL = LibStub("AceLocale-3.0"):NewLocale("GatherMate2Nodes", "enUS", true, debug)
 
 --@localization(locale="enUS", format="lua_additive_table", same-key-is-true=true, namespace="Nodes", table-name="NL")@
+
+-- Midnight Expansion Nodes (Custom)
+NL["Argentleaf"] = true
+NL["Wild Argentleaf"] = true
+NL["Lush Argentleaf"] = true
+NL["Voidbound Argentleaf"] = true
+NL["Lightfused Argentleaf"] = true
+NL["Primal Argentleaf"] = true
+
+NL["Mana Lily"] = true
+NL["Wild Mana Lily"] = true
+NL["Lush Mana Lily"] = true
+NL["Voidbound Mana Lily"] = true
+NL["Lightfused Mana Lily"] = true
+NL["Primal Mana Lily"] = true
+
+NL["Tranquility Bloom"] = true
+NL["Wild Tranquility Bloom"] = true
+NL["Lush Tranquility Bloom"] = true
+NL["Voidbound Tranquility Bloom"] = true
+NL["Lightfused Tranquility Bloom"] = true
+NL["Primal Tranquility Bloom"] = true
+
+NL["Refulgent Copper"] = true
+NL["Refulgent Copper Seam"] = true
+NL["Voidbound Refulgent Copper"] = true
+NL["Lightfused Refulgent Copper"] = true
+NL["Rich Refulgent Copper"] = true
+NL["Primal Refulgent Copper"] = true
+NL["Wild Refulgent Copper"] = true
+
+NL["Umbral Tin"] = true
+NL["Umbral Tin Seam"] = true
+NL["Voidbound Umbral Tin"] = true
+NL["Lightfused Umbral Tin"] = true
+NL["Rich Umbral Tin"] = true
+NL["Primal Umbral Tin"] = true
+NL["Wild Umbral Tin"] = true
+
+NL["Brilliant Silver"] = true
+NL["Brilliant Silver Seam"] = true
+NL["Voidbound Brilliant Silver"] = true
+NL["Lightfused Brilliant Silver"] = true
+NL["Rich Brilliant Silver"] = true
+NL["Primal Brilliant Silver"] = true
+NL["Wild Brilliant Silver"] = true
+
+NL["Hunter Surge"] = true
+NL["Surface Ripple"] = true
+NL["Bubbling Bloom"] = true
+NL["Lost Treasures"] = true
+
